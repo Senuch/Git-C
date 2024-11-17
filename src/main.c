f7509d9897dde3663dc6f173fe3ae90e59cdcea2
Senuch <s.uzairtariq@gmail.com>
2024-11-17 18:27:15 +0500

    Init

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#ifdef _WIN32
+#include <direct.h>
+#define mkdir(path, mode) _mkdir(path)
+#else
 #include <sys/stat.h>
+#endif
 #include <errno.h>
 
 int main(int argc, char *argv[]) {
@@ -17,27 +22,22 @@ int main(int argc, char *argv[]) {
     const char *command = argv[1];
     
     if (strcmp(command, "init") == 0) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        fprintf(stderr, "Logs from your program will appear here!\n");
+        if (mkdir(".git", 0755) == -1 ||
+            mkdir(".git/objects", 0755) == -1 ||
+            mkdir(".git/refs", 0755) == -1) {
+            fprintf(stderr, "Failed to create directories: %s\n", strerror(errno));
+            return 1;
+        }
 
-        // Uncomment this block to pass the first stage
-        // 
-        // if (mkdir(".git", 0755) == -1 || 
-        //     mkdir(".git/objects", 0755) == -1 || 
-        //     mkdir(".git/refs", 0755) == -1) {
-        //     fprintf(stderr, "Failed to create directories: %s\n", strerror(errno));
-        //     return 1;
-        // }
-        // 
-        // FILE *headFile = fopen(".git/HEAD", "w");
-        // if (headFile == NULL) {
-        //     fprintf(stderr, "Failed to create .git/HEAD file: %s\n", strerror(errno));
-        //     return 1;
-        // }
-        // fprintf(headFile, "ref: refs/heads/main\n");
-        // fclose(headFile);
-        // 
-        // printf("Initialized git directory\n");
+        FILE *headFile = fopen(".git/HEAD", "w");
+        if (headFile == NULL) {
+            fprintf(stderr, "Failed to create .git/HEAD file: %s\n", strerror(errno));
+            return 1;
+        }
+        fprintf(headFile, "ref: refs/heads/main\n");
+        fclose(headFile);
+
+        printf("Initialized git directory\n");
     } else {
         fprintf(stderr, "Unknown command %s\n", command);
         return 1;
